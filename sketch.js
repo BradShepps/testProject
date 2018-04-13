8615bc70a93ac3997e559e0ee72d08cb6eb498d4
BradShepps <35384365+BradShepps@users.noreply.github.com>
2018-04-12 21:13:35 -0400

    Made ball moving

--- sketch.js
+++ sketch.js
@@ -1,16 +1,40 @@
-let x = 10;
+let person;
 function setup() {
-	createCanvas(windowWidth, windowHeight, WEBGL);
-
+	createCanvas(800, 800, WEBGL);
+	person = new Person();
 }
 
 function draw() {
-	var c = color(255);
 	background(0);
-	rotateX(x);
-	noStroke();
-	directionalLight(c, -100, -100, -30);
-	ambientMaterial(250);
-	sphere(100);
-	x += .01;
+	person.display();
+	person.walkX(10);
+	person.continuous();
+}
+
+class Person {
+
+	constructor(){
+		this.xspeed = 0;
+		this.yspeed = 0;
+		this.zspeed = 0;
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+	}
+	walkX(spd){
+		this.xspeed = spd;
+		this.x += this.xspeed;
+	}
+	walkY(spd){
+		this.yspeed += spd;
+		this.x += spd;
+	}
+	display(){
+		ellipse(this.x, this.y, 30, 30);
+	}
+	continuous(){
+		if(this.x > width){
+			this.x = -windowWidth+200;
+		}
+	}
 }
